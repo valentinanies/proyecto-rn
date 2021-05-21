2374765c55636a4f6a785b1370646893f5bbc900
Valentina Nies <valunies@gmail.com>
2021-05-21 17:01:51 -0300

    hoja de estilos + estructura app

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react";
+import { styles } from "./Styles";
+import { 
+  Text, 
+  View 
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
 }
+  
+  render(){
+    return(
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text>Hola!</Text>
+
+    )
+  }
+}
---
+++ Styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+
+    stiloText: {
+        color: "white",
+      },
+
+   }) 
+
+   export {styles}
